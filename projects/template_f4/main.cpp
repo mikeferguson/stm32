ecb05f23334469f16d042d4e889838affddc8474
Michael Ferguson <mfergs7@gmail.com>
2012-11-29 02:17:20 -0800

    add linker script, f4 template

---
+++ projects/template_f4/main.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2012, Michael E. Ferguson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * STM32F4XX C++ Template
+ * author: Michael E. Ferguson
+ */
+
+#include "stm32f4xx.h"
+#include "gpio.hpp"
+
+typedef Gpio<GPIOD_BASE,3> act;  // activity LED
+typedef Gpio<GPIOD_BASE,4> stat; // status LED
+
+uint32_t system_clock;
+
+int main(void)
+{
+  NVIC_SetPriorityGrouping(3);
+
+  // enable all GPIO clocks
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+
+  act::mode(GPIO_OUTPUT);
+  stat::mode(GPIO_OUTPUT);
+
+  // setup systick
+  SysTick_Config(SystemCoreClock/1000);
+  NVIC_EnableIRQ(SysTick_IRQn);
+  system_clock = 0;
+
+  __enable_irq();
+
+  while(1)
+  {
+  }
+
+}
+
+extern "C"
+{
+
+void SysTick_Handler(void)
+{
+  system_clock++;
+
+  if( (system_clock%1000) > 500 )
+  {
+    stat::high();
+  }
+  else
+  {
+    stat::low();
+  }
+}
+
+}
